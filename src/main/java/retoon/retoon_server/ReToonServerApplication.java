762cc5ac8d7b3eddc4f8c7e9bf98e2d2c936d8a9
seongjin <nature9545@gmail.com>
2022-07-25 21:51:18 +0900

    initial project

---
+++ src/main/java/retoon/retoon_server/ReToonServerApplication.java
@@ -0,0 +1,13 @@
+package retoon.retoon_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReToonServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReToonServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/retoon/retoon_server/ReToonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package retoon.retoon_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReToonServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
