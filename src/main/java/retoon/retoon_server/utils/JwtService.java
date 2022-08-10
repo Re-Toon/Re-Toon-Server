a0da10d6fb8f0414fc30685c257e115e2cef7a18
uri010 <dbfl0461@gmail.com>
2022-08-10 23:03:16 +0900

    Add verifyJwt to JwtService.java

--- src/main/java/retoon/retoon_server/utils/JwtService.java
+++ src/main/java/retoon/retoon_server/utils/JwtService.java
@@ -1,9 +1,6 @@
 package retoon.retoon_server.utils;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -35,7 +32,7 @@ public class JwtService {
                 .claim("userIdx",userIdx)
                 .setIssuedAt(now)
                 .setExpiration(new Date(System.currentTimeMillis()+ (1000L * 60 * 60 * 24 * 365)))
-                .signWith(key, SignatureAlgorithm.HS256)
+                .signWith(SignatureAlgorithm.HS256, key)
                 .compact();
     }
 
@@ -74,5 +71,25 @@ public class JwtService {
         // 3. userIdx 추출
         return claims.getBody().get("userIdx",Integer.class);
     }
+
+    // Jwt 검증
+    public Boolean verifyJwt(String jwt) {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(Secret.JWT_SECRET_KEY)
+                    .parseClaimsJws(jwt)
+                    .getBody();
+            Long userId = claims.get("userId", Long.class);
+            if (!isValidUser(userId)) return false;
+        } catch (ExpiredJwtException e) {       // 토큰 만료
+            System.out.println(e);
+            return false;
+        } catch (Exception e) {         // 그 외 에러
+            System.out.println(e);
+            return false;
+        }
+        return true;
+    }
+
 
 }
