8c82ccc3383ec0cceac4ab062db94c475e75f45f
shinyumi <yumishin43@gmail.com>
2022-08-16 01:10:38 +0900

    Add User Login Validation

--- src/main/java/retoon/retoon_server/config/BaseResponseStatus.java
+++ src/main/java/retoon/retoon_server/config/BaseResponseStatus.java
@@ -27,7 +27,7 @@ public enum BaseResponseStatus {
     FAILED_ON_SERVER(false, 100, "예기치 못한 에러가 발생했습니다."),
 
     // /users
-    INVALID_USER_IDX(false, 2100, "존재하지 않는 사용자입니다."),
+    NOT_EXIST_USERS(false, 2100, "존재하지 않는 사용자입니다."),
     EMPTY_USER_NICKNAME(false, 2101, "닉네임을 입력해주세요."),
     INSUFFICIENT_USER_GENRE_LIST(false, 2102, "선호하는 장르 4가지를 선택해주세요."),
     NOT_LOGIN_USER(false, 2103, "로그인 되지 않은 사용자입니다."),
@@ -44,18 +44,21 @@ public enum BaseResponseStatus {
     EMPTY_USER_PASSWORD(false, 2108, "비밀번호를 입력해주세요."),
     EMPTY_USER_CHECK_PASSWORD(false, 2109, "비밀번호를 한번 더 입력해주세요."),
     NOT_EQUAL_PASSWORD(false, 2110, "비밀번호가 일치하지 않습니다."),
-    //POST_USERS_INVALID_PASSWORD(false, 2111, "비밀번호는 8~16자의 영문 대/소문자, 숫자, 특수문자를 사용하세요."),
     POST_USERS_INVALID_PASSWORD(false, 2111, "비밀번호는 적어도 1개 이상의 영문, 숫자, 특수문자를 사용하여 8~16자를 입력해주세요."),
+    PASSWORD_ENCRYPTION_ERROR(false, 2112, "비밀번호 암호화에 실패했습니다."),
 
+    // [POST] /users /login
+    FAILED_TO_LOGIN(false, 2113, "로그인에 실패했습니다."),
     /**
      * 3000 : Response 오류
      */
     //Common
-    RESPONSE_ERROR(false, 3000, "값을 불러오는데 실패하였습니다.")
+    RESPONSE_ERROR(false, 3000, "값을 불러오는데 실패하였습니다."),
 
     /**
      * 4000 : Database, Server 오류
      */
+    DATABASE_ERROR(false, 4000, "데이터베이스 오류가 발생했습니다.")
     ;
 
 
