bed6377622e9366008039464558ed2a263a45d6f
shinyumi <yumishin43@gmail.com>
2022-08-15 16:03:06 +0900

    Add User Validation

--- src/main/java/retoon/retoon_server/config/BaseResponseStatus.java
+++ src/main/java/retoon/retoon_server/config/BaseResponseStatus.java
@@ -24,7 +24,23 @@ public enum BaseResponseStatus {
     INVALID_REVIEW_IDX(false, 2006, "존재하지 않는 리뷰입니다."),
     INVALID_REVIEW_USER(false, 2007, "리뷰를 작성한 유저가 아닙니다."),
 
-    FAILED_ON_SERVER(false, 100, "예기치 못한 에러가 발생했습니다.")
+    FAILED_ON_SERVER(false, 100, "예기치 못한 에러가 발생했습니다."),
+
+    // /users
+    INVALID_USER_IDX(false, 2100, "존재하지 않는 사용자입니다."),
+    EMPTY_USER_NICKNAME(false, 2101, "닉네임을 입력해주세요."),
+    INSUFFICIENT_USER_GENRE_LIST(false, 2102, "선호하는 장르 4가지를 선택해주세요."),
+    NOT_LOGIN_USER(false, 2103, "로그인 되지 않은 사용자입니다."),
+
+    /**
+     * 3000 : Response 오류
+     */
+    //Common
+    RESPONSE_ERROR(false, 3000, "값을 불러오는데 실패하였습니다.")
+
+    /**
+     * 4000 : Database, Server 오류
+     */
     ;
 
 
