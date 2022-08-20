b9c362b1eaf76d4217c8dc2c1b3e9fae9af8fd39
shinyumi <yumishin43@gmail.com>
2022-08-20 23:23:30 +0900

    Create Follow JPA Table

---
+++ src/main/java/retoon/retoon_server/src/user/entity/Follow.java
@@ -0,0 +1,33 @@
+package retoon.retoon_server.src.user.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Follow {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int followIdx;
+
+    @JoinColumn(name = "fromUserIdx")
+    @ManyToOne
+    private User fromUser; // 팔로우 하는 사용자
+
+    @JoinColumn(name = "toUserIdx")
+    @ManyToOne
+    private User toUser; // 팔로우 당하는 사용자
+
+    @Builder
+    public Follow(User fromUser, User toUser){
+        this.fromUser = fromUser;
+        this.toUser = toUser;
+    }
+}
