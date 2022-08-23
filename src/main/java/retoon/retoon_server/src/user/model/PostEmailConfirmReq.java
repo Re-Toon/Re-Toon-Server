bdab988a547a5b5b087f4dbf91624ef6c5735b76
shinyumi <yumishin43@gmail.com>
2022-08-23 13:27:45 +0900

    Add Mail Authentication files

---
+++ src/main/java/retoon/retoon_server/src/user/model/PostEmailConfirmReq.java
@@ -0,0 +1,14 @@
+package retoon.retoon_server.src.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostEmailConfirmReq {
+    String email;
+}
