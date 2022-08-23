c0e26ca49d58bb9a94129f6e2bf6ecda45678e6e
shinyumi <yumishin43@gmail.com>
2022-08-23 13:28:34 +0900

    Add Spring Security Settings

---
+++ src/main/java/retoon/retoon_server/config/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package retoon.retoon_server.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import retoon.retoon_server.src.login.filter.JwtAuthenticationFilter;
+import retoon.retoon_server.src.login.handler.CustomAccessDeniedHandler;
+import retoon.retoon_server.src.login.handler.CustomAuthenticationEntryPoint;
+import retoon.retoon_server.src.login.token.JwtTokenProvider;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    // 패스워드 인코더
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable() // rest api이므로 기본설정 미사용
+                .csrf().disable() // rest api이므로 csrf 보안 미사용
+                .formLogin().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt로 인증하므로 세션 미사용
+                .and()
+                .authorizeRequests()
+                .antMatchers("/sign/**").permitAll()
+                .antMatchers("/social/**").permitAll()
+                .antMatchers("/exception/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .and()
+                .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class); // jwt 필터 추가
+    }
+}
