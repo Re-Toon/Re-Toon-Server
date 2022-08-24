25773396a817dfd979c8e9d72cc6653b917245f5
uri010 <dbfl0461@gmail.com>
2022-08-25 00:10:15 +0900

    Add GetReviewListRes.java

---
+++ src/main/java/retoon/retoon_server/src/review/model/GetReviewListRes.java
@@ -0,0 +1,42 @@
+package retoon.retoon_server.src.review.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import retoon.retoon_server.src.review.entity.Review;
+
+import java.time.LocalDateTime;
+import java.util.List;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetReviewListRes {
+    private String nickname;
+    private String reviewText;
+    private int reviewStarRate;
+    private boolean spoiler;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime updatedAT;
+
+    private int reviewLikeCnt;
+    private int reviewUnlikeCnt;
+    private int commentCnt;
+
+    /*Entity ->Dto*/
+    public GetReviewListRes(Review review) {
+        this.nickname = review.getUser().getNickname();
+        this.reviewText = review.getReviewText();
+        this.reviewStarRate = review.getReviewStarRate();
+        this.updatedAT = review.getUpdatedAT();
+        this.reviewLikeCnt = review.getReviewLikes().size();
+        this.reviewUnlikeCnt = review.getReviewUnlikes().size();
+        this.commentCnt = review.getComments().size();
+    }
+
+
+}
+
